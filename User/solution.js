a1cba1be0e20b57e46dbc4218fc856acfd8ba959
itzpritish4u <rajpritish75@gmail.com>
2024-09-08 22:58:05 +0530

    Formed all the functions in solution

--- User/solution.js
+++ User/solution.js
@@ -1,10 +1,11 @@
-import { users } from './users';
 // To find users who are interested in playing video games.
 const getUsersInterestedInVideoGames = (users) => {
+    const result = [];
     for (let user in users) {
-        if (users[user].interests && users[user].interests[0].includes("Playing Video Games")) {
-            console.log(user);
+        if (users[user].interests[0].includes("Playing Video Games")) {
+            result.push(user);
         }
+        return result;
     }
 }
 
@@ -14,6 +15,42 @@ const findUsersInGermany = (users) => {
     for (let user in users) {
         if (users[user].nationality === "Germany") {
             result.push(user);
+        }
+    }
+    return result;
+}
+
+// To find all users with masters Degree.
+const  findUsersWithMastersDegree = (users) => {
+    const result = [];
+    for (let  user in users) {
+        if (users[user].qualification === "Masters") {
+            result.push(user);
+        }
+    }
+    return result;
+}
+
+// To group users based on their Programming language mentioned in their designation.
+const groupUsersByProgrammingLanguage = (users) => {
+    const result = {
+        Java: [],
+        Python: [],
+        JavaScript: [],
+        Golang: [],
+        OtherDeveloper: []
+    }
+    for (let user in users) {
+        if (users[user].designation.toLoweCase().includes("python")) {
+            result.Python.push(user);
+        }  else if (users[user].designation.toLoweCase().includes("java")) {
+            result.Java.push(user);
+        } else if (users[user].designation.toLoweCase().includes("javascript")) {
+            result.JavaScript.push(user);
+        } else if (users[user].designation.toLoweCase().includes("golang")) {
+            result.Golang.push(user);
+        } else {
+            result.OtherDeveloper.push(user);
         }
     }
     return result;
--- User/test.js
+++
@@ -1,9 +0,0 @@
-import {users} from './users.js';
-import {getUsersInterestedInVideoGames, findUsersInGermany}  from './solution.js';
-
-// Test for getUsersInterestedInVideoGames
-const testGetUsersInterestedInVideoGames = () => {
-    console.log(getUsersInterestedInVideoGames(users));
-}
-
-testGetUsersInterestedInVideoGames();
